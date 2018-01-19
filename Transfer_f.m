c52086dcdd7031fce6106c0037d6db8c7fc8469f
matemarinkovic <matemarinkovic@me5174-05.ad.doe.carleton.ca>
2018-01-19 17:07:00 -0500

    Updated parameters for 2V supply for correct biasing

--- Transfer_f.m
+++ Transfer_f.m
@@ -2,44 +2,44 @@ function Transfer_f = Transfer_f(f,wave)
 
 %2.0V VDD and 0.4V SUP
 if strcmp(wave,'Sine')
-    Y=1/50;
+    Y = 1/50;
     Ron_p = 44.95;
     Cds_p = 196.8061e-15;
-    Cgd2 = 81.2943e-15;
-    Cgs3 = 85.9043e-15;
-    Cds2 = 186.3112e-15;
-    Cds3 = 187.5755e-15;
-    Ron3 = 14.4433;
-    ro2 = 834.5852;
-    Cgd3 = 82.1576e-15;
-    Cgs2 = 85.3737e-15;
-    gm2 = 1.1982e-3;
+    Cgd2 = 23.4052e-15;
+    Cds2 = 94.9907e-15;
+    Cgs2 = 104.4758e-15;
+    gm2 = 18.3618e-3;
+    ro2 = 1/gm2;
+    Ron3 = 15.107;
+    Cgs3 = 86.9028e-15;
+    Cds3 = 183.7823e-15;
+    Cgd3 = 81.0317e-15;
 elseif strcmp(wave,'Sawtooth') %18.6ps on rising edge in hspice
     Y = 1/50;
-    Ron_p = 101.02;
-    Cds_p = 195.4424e-15;
-    Cgd2 = 42.8858e-15;
-    Cgs3 = 84.5710e-15;
-    Cds2 = 138.8406e-15;
-    Cds3 = 180.6806e-15;
-    Ron3 = 39.00;
-    gm2 = 6.9541e-3;
+    Ron_p = 75.739;
+    Cds_p = 195.9636e-15;
+    Cgd2 = 19.2048e-15;
+    Cds2 = 83.3662e-15;
+    Cgs2 = 100.3848e-15;
+    gm2 = 13.7369e-3;
     ro2 = 1/gm2;
-    Cgd3 = 76.7787e-15;
-    Cgs2 = 96.9478e-15;
+    Ron3 = 28.15;
+    Cgs3 = 85.5391e-15;
+    Cds3 = 181.4708e-15;
+    Cgd3 = 78.1194e-15;
 elseif strcmp(wave,'Square')
-    Y=1/50;
-    Ron_p = 46.7053;
-    Cds_p = 196.744e-15;
-    Cgd2 = 80.9480e-15;
-    Cds2 = 185.8157e-15;
-    gm2 = 1.3867e-3;
+    Y = 1/50;
+    Ron_p = 44.95;
+    Cds_p = 196.8061e-15;
+    Cgd2 = 23.4052e-15;
+    Cds2 = 94.9907e-15;
+    Cgs2 = 104.4758e-15;
+    gm2 = 18.3618e-3;
     ro2 = 1/gm2;
-    Cgs2 = 85.2329e-15;
-    Cgs3 = 85.7850e-15;
-    Cds3 = 187.328e-15;
-    Ron3 = 15.16;
-    Cgd3 = 81.9805e-15;
+    Ron3 = 15.107;
+    Cgs3 = 86.9028e-15;
+    Cds3 = 183.7823e-15;
+    Cgd3 = 81.0317e-15;
 end
 omega = 2*pi*f;
 
